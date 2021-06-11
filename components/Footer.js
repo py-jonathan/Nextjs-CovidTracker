d5275fb6df2ba4d15f72e12c4b0b5584ea922c83
py-jonathan <pyjonpk@gmail.com>
2021-06-11 19:37:06 +0545

    modified footer text

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <h5>
-        {new Date().getFullYear()} <sup>&copy;</sup>. {"  "}All rights reserved by COVID
-        TRACKER
+        Made with ♥ by Sajjan - {new Date().getFullYear()}
+        <sup>&copy;</sup>
       </h5>
       {/* <p>Developer: <a href="https://instagram.com/sajjan_404" target="_blank" className={`btn btn-success ${styles.btn_dev}`}>Sajjan</a></p> */}
     </footer>
